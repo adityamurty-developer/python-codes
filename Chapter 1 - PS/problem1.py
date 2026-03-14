0a8ec25305f74b86190c21521bcf7371f1490fc2
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-14 23:01:55 +0530

    first commit

---
+++ Chapter 1 - PS/problem1.py
@@ -0,0 +1,5 @@
+# Program to print your introduction
+
+print('''My name is Aditya. I am pursuing my B.Tech in CSE branch and currently I am in my 2nd year. I am passionate about becoming a Backend as well as an AI developer. 
+      
+Currently, I have skills in C, C++, DSA basics, HTML, and CSS. Right now I am also learning Python from CWH (Code With Harry). My hobbies are playing cricket, listening to music, and travelling.''')
---
+++ Chapter 1/comments.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+# print("printing jokes...")
+joke = pyjokes.get_joke()
+print(joke)
+
+# hey so thanks 
+# that was my program
+# and let's move to
+# the new python chapter
