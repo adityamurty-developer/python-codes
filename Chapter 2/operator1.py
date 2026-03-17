27ae92861de70c35d6336397d03a57ac399891e9
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-18 00:22:18 +0530

    Operators

--- Chapter 2/operator1.py
+++ Chapter 2/operator1.py
@@ -4,5 +4,30 @@ a = 7
 b = 8
 
 c = a + b
-
 print(c)
+
+# Assignment Operators
+
+a = 4-2  # assign 4-2 in a
+print(a)
+b = 6
+# b += 3  # Increment the value of b by 3 and then assign it to b
+b -= 3  # Decrement the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+
+d = 5>=4
+print(d)
+e = 5!=5
+print(e)
+
+# Logical Operators
+
+e = True or False
+print(e)
+
+f = True and False
+print(f)
+
+print(not(False))
---
+++ Chapter 2/operator2.py
@@ -0,0 +1,15 @@
+# Logical 'OR' Operator
+e = True or False
+
+print("False or False is: ", False or False)
+print("False or True is: ", False or True)
+print("True or False is: ", True or False)
+print("True or True is: ", True or True)
+
+
+# Logical 'AND' Operator
+
+print("False and False is: ", False and False)
+print("False and True is: ", False and True)
+print("True and False is: ", True and False)
+print("True and True is: ", True and True)
