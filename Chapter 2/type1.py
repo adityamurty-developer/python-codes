274c0a89c1c0f7dc8f6f8a0fbeb0723fbae483b0
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-18 18:55:18 +0530

    Typecasting

---
+++ Chapter 2/type1.py
@@ -0,0 +1,19 @@
+a = 31
+t = type(a)  # class <int>
+
+print(t)
+
+b = 31.2
+t = type(b)   # class <float>
+
+print(t)
+
+c = "Adi"
+t = type(c)   # class <str>
+
+print(t) 
+
+d = True or False
+t = type(d)   # class <bool>
+
+print(t)
