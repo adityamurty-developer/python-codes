0af7bc6b5f540d4e7e633150735904ac974736b1
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-18 19:06:01 +0530

    Typecasting

---
+++ Chapter 2/type2.py
@@ -0,0 +1,12 @@
+a = "31.2"
+b = float(a)  # a but type should be float
+t = type(b)
+
+print(t)    # class <float>
+
+c = 24
+d = str(c)   # c but type should be string
+t = type(d)
+
+print(t)
+  
