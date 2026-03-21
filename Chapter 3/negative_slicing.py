003b3a1a96bf995526d8d31bc98f727fd4868cda
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-21 19:37:59 +0530

    Slicing with skip value

---
+++ Chapter 3/negative_slicing.py
@@ -0,0 +1,10 @@
+name = "Aditya"
+
+print(name[0:3])
+
+print(name[-4:-1])    # negative slicing
+print(name[2:5])    # positive slicing
+
+print(name[:4])   # if no start index is written then it is consider as 0
+
+print(name[0:])   # if no end index is written then it is consider as (length-1)
---
+++ Chapter 3/slicing_with_skip.py
@@ -0,0 +1,11 @@
+num = "012345678"
+
+s1 = num[0:8:2]   # output: 0246
+
+print(s1)
+
+word = "amazing"
+
+s2 = word[1:6:2]    # output: mzn
+
+print(s2)
