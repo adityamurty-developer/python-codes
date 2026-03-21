ff9b046505d35c39fcdc4a148d4a261462b76c65
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-21 18:16:53 +0530

    String indexing

---
+++ Chapter 3/string_slicing.py
@@ -0,0 +1,8 @@
+name = "Aditya"   # indexing starts from 0 to (length - 1)
+
+nameshort = name[0:3]  # start from index 0 all the way till 3 (excluding 3)
+
+print(nameshort)
+
+char1 = name[1]
+print(char1)
