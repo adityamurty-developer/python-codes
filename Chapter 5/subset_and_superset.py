22053fb2b56ccd7cc3899520baa91de0ca73efa9
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-23 22:40:55 +0530

    Subset and Superset operations

---
+++ Chapter 5/subset_and_superset.py
@@ -0,0 +1,8 @@
+a = {1, 2}
+b = {1, 2, 3, 4}
+
+# a is subset of b if all elements of a are in b
+print(a.issubset(b))
+
+# b is superset of a if b contains all elements of a
+print(b.issuperset(a))
