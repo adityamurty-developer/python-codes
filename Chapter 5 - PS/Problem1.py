a8538f894bf549ea04ade86059463be48d32c493
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-23 23:47:43 +0530

    Practice Problem 1

---
+++ Chapter 5 - PS/Problem1.py
@@ -0,0 +1,11 @@
+words = {
+    "madad": "Help",
+    "kursi": "Chair",
+    "paani": "Water",
+    "kutta": "Dog",
+    "nashta": "Breakfast",
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
