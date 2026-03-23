93f3edd144d52664d4f0d4e0606795c0a014a4b4
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-24 00:22:23 +0530

    Problem 7

---
+++ Chapter 5 - PS/Problem7.py
@@ -0,0 +1,23 @@
+# If tha names of two friends are same; what will happen to the program in problem 6
+
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter your favorite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter your favorite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter your favorite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter your favorite language: ")
+d.update({name: lang})
+
+print(d)
+
+# if two names are same then the last entered value/lang is taken for that name
