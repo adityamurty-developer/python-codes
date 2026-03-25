d86567d811f1c0b278f1046aec7f420e43fa0484
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-25 22:37:41 +0530

    if else

---
+++ Chapter 6/if_else.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if(age>=18):
+    print("You are above the age consent")
+    print("Good for you")
+
+else:
+    print("You are below the age consent")
+
+print("End of Program")
