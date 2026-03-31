8baafff33b410dda3a8821db023c8079ef3e7a0f
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-31 12:54:50 +0530

    pass statement

---
+++ Chapter 7/pass.py
@@ -0,0 +1,9 @@
+for i in range(40):
+    pass  
+# pass is a null statement which instruct to do nothing. 
+# If pass is not used here then the compiler will give indentation error. 
+
+i = 0
+while(i<11):
+    print(i)
+    i+=1
