cdc62f21690fb1094f01b4a205375a30053ef836
R Aditya Murty <adityamurty2006@gmail.com>
2026-03-31 11:01:43 +0530

    For loop

---
+++ Chapter 7/loops1.py
@@ -0,0 +1,10 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# instead of repeating these statements we use loops
+
+for i in range(1,6):   # range() is function used to generate a squence of a number
+    print(i)
