a2564b37a353e945c1fe7ecd7a3cf3b2b2a8e178
R Aditya Murty <adityamurty2006@gmail.com>
2026-04-06 10:59:39 +0530

    arguments in python

---
+++ Chapter 8/arguments_in_function2.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x * x
+
+print(square(2))    # first 2 will be passed to x and then we will get the result --> 4
+print(square(5))    # next 5 will be passed and the output will be --> 25
