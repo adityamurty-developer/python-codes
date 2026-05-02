d957c14547d85957d8a465bca32571780a333aed
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-02 22:32:23 +0530

    Problem 9

---
+++ Chaprer 9 - PS/Problem9.py
@@ -0,0 +1,10 @@
+with open("this.txt") as f:
+    content1 = f.read()
+
+with open("this_copy.txt") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Yes, files are identical")
+else:
+    print("No, they are not identical")
