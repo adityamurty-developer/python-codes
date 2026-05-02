f2d8695adc77e00ad0e8e1dbe52bdb77e03f953e
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-02 22:27:45 +0530

    Problem 8

--- Chaprer 9 - PS/Problem7.py
+++ Chaprer 9 - PS/Problem7.py
@@ -2,7 +2,7 @@ with open("log.txt") as f:
     lines = f.readlines()
 
 lineno = 1
-# this loops check if python is present in any of the line
+# this loops check if "python" is present in any of the line
 for line in lines:
     if("Python" in line):
         print(f"Yes Python is present in line: {lineno}")
