322ac98cf5babf714d94515cda373fe190c3a124
R Aditya Murty <adityamurty2006@gmail.com>
2026-06-01 17:31:41 +0530

    __name__ == __main__ updated

--- Chapter 12/module.py
+++ Chapter 12/module.py
@@ -1,5 +1,8 @@
 def myFunc():
     print("Hello World")
 
-myFunc()
-print(__name__)
+if __name__ == "__main__":
+    # if this code is directly executed by running the file its present in!
+    print("We are directly running this code")  
+    myFunc()
+    print(__name__)
