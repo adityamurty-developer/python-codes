23034aae0b43260c3975616e3bf4aa41c46df51f
R Aditya Murty <adityamurty2006@gmail.com>
2026-06-01 10:14:59 +0530

    removed Chapter 12 folder files

--- Chapter 12/exception.py
+++
@@ -1,8 +0,0 @@
-try:
-    a = int(input("Enter a number: "))
-    print(a)
-
-except Exception as e:
-    print(e)
-
-print("Thank You")
--- Chapter 12/walrus.py
+++
@@ -1,3 +0,0 @@
-# using walrus operator
-if(n := len([1, 2, 3, 4, 5])) > 3:
-    print(f"List is too long ({n} elements, expected <= 3)")
