92eaa6849c8f6f01883a128b98df7c70a19cd828
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-23 11:24:36 +0530

    python revision

---
+++ Py-revision/practice1.py
@@ -0,0 +1,29 @@
+# Create variables for:
+
+# your name
+# age
+# height
+# college name
+# whether you are a student
+
+# Print each value along with its datatype using type().
+
+name = "Aditya"
+print(name)
+print(type(name))
+
+age = 20
+print(age)
+print(type(age))
+
+height = '6 ft'
+print(height)
+print(type(height))
+
+college_name = "BGIEM"
+print(college_name)
+print(type(college_name))
+
+is_student = True
+print(is_student)
+print(type(is_student))
