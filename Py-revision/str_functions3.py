2808506aff9422f57d65b8bbff9f894c27762b12
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 22:14:06 +0530

    str functions

---
+++ Py-revision/str_functions3.py
@@ -0,0 +1,16 @@
+# Searching and Checking Start / End
+
+text1 = "Hello Python"
+
+print(text1.find("Python"))
+print(text1.find("java"))
+
+text2 = "Banana"
+print(text2.count("a")) # .count() --> how many time does the char/substring occurs
+
+text3 = "Python Programming"
+print(text3.startswith("Python"))
+print(text3.startswith("Cpp"))
+
+print(text3.endswith("ing")) 
+
