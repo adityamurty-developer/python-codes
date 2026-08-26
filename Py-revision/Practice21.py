e56e61dedf7f3f4aac0429eeaed61bc925fee0f6
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 18:51:38 +0530

    Practice 21

---
+++ Py-revision/Practice21.py
@@ -0,0 +1,20 @@
+# String Repetition
+
+word = "Hello"
+print((word + " ") * 3)  # prints "Hello" 3 times
+
+print("")
+
+# String in / not in --> checks if any char / substring is present inside a string or not
+
+word1 = "Python"
+
+print("P" in word1)
+print("z" in word1)
+print("z" not in word1)
+
+word2 = "Programming language"
+
+print("language" in word2)
+
+print("")
