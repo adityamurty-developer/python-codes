f9dc470028267cba91d356be766d3b0dc5c73d81
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 13:24:06 +0530

    Practice 15

---
+++ Py-revision/Practice15.py
@@ -0,0 +1,5 @@
+# Write a Py program to display a user entered name followed by Good Afternoon using input() function
+
+name = input("Hey, what is your name: ")
+
+print(f"Hii {name}, Good Afternoon!")
