707fafe1c5ca04c13b7eaa6f00bb5e9b1cc58ce9
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 20:01:22 +0530

    Practice 23

---
+++ Py-revision/Practice23.py
@@ -0,0 +1,16 @@
+# Escape Sequence 
+# \n  -->  New line
+# \t  -->  Tab space
+# \"  -->  Double quote
+# \'  -->  Single quote
+# \\  -->  Backslash
+
+print("Hello\nWorld")
+
+print("Name:\tAditya")
+
+print("He said, \"Hello\"")
+
+print("It\'s Python")
+
+print("Cpp\\Python\\Java")
