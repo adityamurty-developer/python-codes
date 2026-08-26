4bd22351db9c1ebfea6676ee2c9438164b6d7c8c
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 12:14:16 +0530

    Practice 12

---
+++ Py-revision/Practice12.py
@@ -0,0 +1,7 @@
+# You are given a word - Python and you have to print Pyt using string slicing
+
+word = "Python"
+
+print(word[0:2])  # Here Py is print instead of Pyt because end index is excluded and all the indexe's before end index is included from start index
+
+print(word[0:3]) # Here Pyt is printed from 0 - 2 index and end index i.e. 3 is excluded
