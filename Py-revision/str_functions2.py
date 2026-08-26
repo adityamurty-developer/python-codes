74df818af88744cbc13073ab88fdcd13efcf906d
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 22:05:30 +0530

    str functions

---
+++ Py-revision/str_functions2.py
@@ -0,0 +1,8 @@
+# Whitespace Cleaning
+
+text = "  Python  "
+print(text.strip()) # .strip() --> removes whitespace from beginning and end 
+
+print(text.lstrip()) # .lstrip() --> removes whitespace from beginning
+
+print(text.rstrip()) # .rstrip() --> removes whitespace from end
