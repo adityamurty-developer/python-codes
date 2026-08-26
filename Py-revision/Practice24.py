19be8ced765a7f41c63e02283b7898e83479c715
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 21:59:23 +0530

    string functions

---
+++ Py-revision/Practice24.py
@@ -0,0 +1,13 @@
+# Strings Functions: Case Conversion
+
+text1 = "PyThon"
+print(text1.lower()) # convert the text into lowercase
+
+print(text1.upper()) # convert the text into uppercase
+
+print("")
+
+text2 = "hello world"
+print(text2.capitalize()) # make's only first char uppercase
+
+print(text2.title()) # capitalize first char of each word
