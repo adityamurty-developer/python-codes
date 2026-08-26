e354205cc2f98d4b067ff29fa4e959f11173a906
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-26 18:16:50 +0530

    Practice 20

---
+++ Py-revision/Practice19.py
@@ -0,0 +1,8 @@
+# String Concatenation 
+
+str1 = "Hello"
+str2 = "World"
+
+print(str1 + " " + str2)   # string concatenation method
+
+print(f"{str1} {str2}")  # f-string method used in modern python
