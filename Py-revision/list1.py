1a743bb7fee2e08ba5217a9de0e31ec1db6708fd
R Aditya Murty <adityamurty2006@gmail.com>
2026-08-27 10:41:11 +0530

    list revision

---
+++ Py-revision/list1.py
@@ -0,0 +1,18 @@
+# List --> stores multiple value's inside a variable
+
+l1 = ["Aditya", "Amit", "Shreya"]
+print(l1)
+
+# list can store same and different data types 
+data = ["Aditya", 20, 7.6, True]
+
+# Duplicate value's are allowed in a list 
+nums = [20, 10, 20, 30]
+
+# list are ordered so we can do list indexing
+l2 = [10, 20, 30]
+print(l2[0], l2[1], l2[2])
+
+# list is mutable so, we can change existing element of a list 
+l2[0] = 100
+print(l2)
