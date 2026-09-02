8476da6f2b68edd5527f73e44d9ae87fb252de43
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-02 22:26:26 +0530

    sets

---
+++ Py-revision/sets1.py
@@ -0,0 +1,19 @@
+# set --> it is a collection of unique values
+nums = {10, 20, 30, 10}
+print(nums)  # 10 automatically removes because its a duplicate value
+
+# properties of a set
+#  It is mutable
+# It is unordered
+# Duplicates not allowed
+# Indexing not supported
+# Different data types allowed
+
+data = {10, "Pyhton", "8.2", True}
+print(data)
+
+# print(data[0]) --> we cannot do indexing because set is unordered
+
+# Creating an empty set 
+nums = set() # this is a empty set
+nums = {} # this is an empty dictionary
