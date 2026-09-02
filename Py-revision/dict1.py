404b79b672b52600302bf53a57c20e9d57d17892
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-02 19:22:18 +0530

    dictionary

---
+++ Py-revision/dict1.py
@@ -0,0 +1,26 @@
+# Dictionary --> stores data in key-value pair
+
+student = {
+    "name": "Rohan",
+    "age": 20,
+    "cgpa": 7.6,
+    "skills": ["Py", "Django", "Cpp", "Dsa"]
+}
+
+print(student)
+
+# Properties of Dictionary:
+
+# It is mutable
+# Keys should be unique
+# Values can be duplicate
+# Values can be accessed using key
+# Different data types values can be stored
+# It is unordered
+
+# Accessing values 
+
+print(student["name"])
+print(student["age"])
+
+# if key not exist then we will get a keyError
