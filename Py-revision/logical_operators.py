69673f08dc8303469ddcdf96e616358da5c8dfb4
R Aditya Murty <adityamurty2006@gmail.com>
2026-09-03 18:13:50 +0530

    logical operators

---
+++ Py-revision/logical_operators.py
@@ -0,0 +1,19 @@
+# AND operator --> both results should true
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print(" ")
+
+# OR operator --> atleast one should true
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print(" ")
+
+# NOT operator --> it reverse the result 
+print(not True)
+print(not False)
